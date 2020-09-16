40d41f5d3944fce64ab9e82cc25c50144c0abf15
Michael Madumere <you@example.com>
2020-09-17 00:04:04 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'})
 const app = express();
+
+mongoose.connect(process.env.LOCAL_DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(()=>{ 
+    console.log(`MongoDb connected`)
+}).catch(err =>{
+    console.log(`Database connection error => ${err}`)
+});
+
+// Middlewares
+app.use(morgan('dev'));
 
 app.get('/', (req, res) =>{
     res.send('Hello world')
 });
-const port = 8080;
-app.listen(port, () =>{
-    console.log(`Buynow running on port ${port}`)
+const PORT = process.env.PORT;
+app.listen(PORT, () =>{
+    console.log(`⚡️[Server]: Buynow server running on port http://localhost:${PORT}`)
 })
